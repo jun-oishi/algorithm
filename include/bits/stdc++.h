ac9f574cd687d11888dfde17acaf5b06d72caf7c
jun-oishi <oishi.junnosuke@gmail.com>
2023-06-17 06:33:04 +0900

    add cmath in stdc++.h

--- include/bits/stdc++.h
+++ include/bits/stdc++.h
@@ -1,19 +1,20 @@
 #include <algorithm>  // min, max, swap, sort, reverse, lower_bound, upper_bound
 #include <bitset>     // bitset
 #include <cctype>     // isupper, islower, isdigit, toupper, tolower
-#include <cstdint>    // int64_t, int*_t
-#include <cstdio>     // printf
-#include <deque>      // deque
-#include <iostream>   // cout, endl, cin
-#include <map>        // map
-#include <queue>      // queue, priority_queue
-#include <set>        // set
-#include <stack>      // stack
-#include <string>     // string, to_string, stoi
-#include <tuple>      // tuple, make_tuple
+#include <cmath>
+#include <cstdint>        // int64_t, int*_t
+#include <cstdio>         // printf
+#include <deque>          // deque
+#include <iomanip>        // setprecision, fixed
+#include <iostream>       // cout, endl, cin
+#include <list>           // list
+#include <map>            // map
+#include <queue>          // queue, priority_queue
+#include <set>            // set
+#include <stack>          // stack
+#include <string>         // string, to_string, stoi
+#include <tuple>          // tuple, make_tuple
 #include <unordered_map>  // unordered_map
 #include <unordered_set>  // unordered_set
 #include <utility>        // pair, make_pair
 #include <vector>         // vector
-#include <list>           // list
-#include <iomanip>        // setprecision, fixed
